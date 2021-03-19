5ad5ad9dc830bb7e5ff77fc0bda33112aba12b55
Alvaro Ortega Marmol <alortegama@outlook.com>
2021-03-19 20:15:24 +0100

    Clase #15 Viernes 19/03

---
+++ navigation_app/app/src/main/java/com/codigonline/curso_navigation/ui/fragments/ProductoViewModel.kt
@@ -0,0 +1,177 @@
+package com.codigonline.curso_navigation.ui.fragments
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.codigonline.curso_navigation.models.Producto
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+
+
+class ProductoViewModel : ViewModel() {
+
+    /**
+     * MutableLiveData<List<Producto>> productos = new MutableLiveData<>()
+     *  val productos = MutableLiveData<List<Producto>>()
+
+     */
+
+    private val _productos: MutableLiveData<List<Producto>> by lazy {
+        // also --> Quiero hacer algo más, en este caso introducir datos
+        MutableLiveData<List<Producto>>().also {
+            cargarProductos()
+        }
+    }
+
+    private fun cargarProductos() {
+
+        //SIMULANDO LA CONEXIÓN A LA BD
+
+        viewModelScope.launch {
+            withContext(Dispatchers.IO) {
+                val productos = mutableListOf<Producto>() //LISTA MUTABLE
+                productos.add(
+                    Producto(
+                        1,
+                        "individual-mini",
+                        "Clases individuales de repaso en informática tamaño mini, 2 clases ",
+                        59.99,
+                        2
+                    )
+                )
+                productos.add(
+                    Producto(
+                        1,
+                        "individual-basic",
+                        "Clases individuales de repaso en informática tamaño basic, 4 clases ",
+                        79.99,
+                        4
+                    )
+                )
+                productos.add(
+                    Producto(
+                        3,
+                        "individual-premium",
+                        "Clases individuales de repaso en informática tamaño premium, 10 clases ",
+                        119.99,
+                        10
+                    )
+                )
+                productos.add(
+                    Producto(
+                        4,
+                        "grupo-mini",
+                        "Clases en grupo de repaso en informática tamaño mini, 2 clases ",
+                        39.99,
+                        2
+                    )
+                )
+                productos.add(
+                    Producto(
+                        5,
+                        "grupo-basic",
+                        "Clases en grupo de repaso en informática tamaño basic, 4 clases ",
+                        59.99,
+                        4
+                    )
+                )
+                productos.add(
+                    Producto(
+                        6,
+                        "grupo-premium",
+                        "Clases en grupo de repaso en informática tamaño premium, 10 clases ",
+                        99.99,
+                        10
+                    )
+                )
+                productos.add(
+                    Producto(
+                        7,
+                        "proyecto-final",
+                        "Tutorización del proyecto de final de grado. 8 clases para conseguir el proyecto que siempre has deseado ",
+                        99.99,
+                        8
+                    )
+                )
+                productos.add(
+                    Producto(
+                        8,
+                        "PAC-M03A",
+                        "Ayuda en la realización de la PAC de desarrollo de M03A ",
+                        49.99,
+                        2
+                    )
+                )
+                productos.add(
+                    Producto(
+                        9,
+                        "PAC-M03B",
+                        "Ayuda en la realización de la PAC de desarrollo de M03B ",
+                        49.99,
+                        2
+                    )
+                )
+                productos.add(
+                    Producto(
+                        10,
+                        "PAC-DAM-M06",
+                        "Ayuda en la realización de la PAC de desarrollo de DAM-M06 ",
+                        49.99,
+                        2
+                    )
+                )
+                productos.add(
+                    Producto(
+                        11,
+                        "PAC-DAM-M07",
+                        "Ayuda en la realización de la PAC de desarrollo de DAM-M07",
+                        49.99,
+                        2
+                    )
+                )
+                productos.add(
+                    Producto(
+                        12,
+                        "PAC-DAM-M08",
+                        "Ayuda en la realización de la PAC de desarrollo de DAM-M08",
+                        49.99,
+                        2
+                    )
+                )
+                productos.add(
+                    Producto(
+                        13,
+                        "PAC-DAM-M09",
+                        "Ayuda en la realización de la PAC de desarrollo de DAM-M09",
+                        49.99,
+                        2
+                    )
+                )
+                productos.add(
+                    Producto(
+                        14,
+                        "curso-android-1S-2021",
+                        "Curso de Android + Kotlin, inicio febrero de 2021",
+                        29.99,
+                        0
+                    )
+                )
+                _productos.postValue(productos)  //MAIN THREAD
+            }
+
+        }
+
+
+
+    }
+
+    /* fun obtenerProductos() = _productos*/
+    // PROTECCION PARA EVITAR QUE SE MODIFIQUEN LOS DATOS QUE ENVIAMOS
+    fun obtenerProductos(): LiveData<List<Producto>> {
+        return _productos;
+    }
+
+
+}
--- navigation_app/app/src/main/java/com/codigonline/curso_navigation/ui/fragments/ProductosFragment.kt
+++ navigation_app/app/src/main/java/com/codigonline/curso_navigation/ui/fragments/ProductosFragment.kt
@@ -8,6 +8,8 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.Toast
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -25,8 +27,7 @@ class ProductosFragment : Fragment() {
 
     private var listener: MainListener? = null
     private var _binding: FragmentProductosBinding? = null
-    private lateinit var mAdapter:RecyclerViewAdapter
-
+    private lateinit var mAdapter: RecyclerViewAdapter
 
 
     override fun onCreateView(
@@ -37,11 +38,23 @@ class ProductosFragment : Fragment() {
         _binding = FragmentProductosBinding.inflate(inflater, container, false)
         val binding = _binding!!
         val view = binding.root
-        //createListView()
-        createRecyclerView()
+        // createListView()
+        // createRecyclerView()
+
+        //val model = ViewModelProvider(this).get(ProductoViewModel::class.java)
+        val model: ProductoViewModel by viewModels()
+
+        model.obtenerProductos().observe(viewLifecycleOwner, { //--> RECIBO NOTIFICACIÓN DE DATOS NUEVOS
+            createRecyclerView(it)
+        })
+
+
+
+
+
 
         binding.productosFab.setOnClickListener {
-            mAdapter.crearProducto(Producto(15L,"Nuevo producto","Nueva descripción",345678.0,2))
+            mAdapter.crearProducto(Producto(15L, "Nuevo producto", "Nueva descripción", 345678.0, 2))
         }
 
 
@@ -52,8 +65,8 @@ class ProductosFragment : Fragment() {
 
     }
 
-    private fun createRecyclerView() {
-        mAdapter = RecyclerViewAdapter(cargarProductos() as MutableList<Producto>)
+    private fun createRecyclerView(productos: List<Producto>) {
+        mAdapter = RecyclerViewAdapter(productos as MutableList<Producto>)
         val recyclerView = _binding!!.productosRecyclerView
         recyclerView.apply {
             //EL RECYCLER VIEW VA A SER UNA LISTA VERTICAL
@@ -63,7 +76,7 @@ class ProductosFragment : Fragment() {
         }
     }
 
-    private fun createListView() {
+    private fun createListView(productos: List<Producto>) {
         //CONTEXT
         //LAYOUT DONDE CARGAR LOS DATOS
         //LOS DATOS
@@ -72,30 +85,8 @@ class ProductosFragment : Fragment() {
                  android.R.layout.simple_list_item_1,
                  cargarProductos()
          )*/
-        val adapter = ListViewAdapter(requireContext(), R.layout.item_producto_list_view, cargarProductos())
+        val adapter = ListViewAdapter(requireContext(), R.layout.item_producto_list_view, productos)
         //_binding!!.productosListView.adapter = adapter
-    }
-
-    private fun cargarProductos(): List<Producto> {
-        val productos = mutableListOf<Producto>() //LISTA MUTABLE
-
-        productos.add(Producto(1, "individual-mini", "Clases individuales de repaso en informática tamaño mini, 2 clases ", 59.99, 2))
-        productos.add(Producto(1, "individual-basic", "Clases individuales de repaso en informática tamaño basic, 4 clases ", 79.99, 4))
-        productos.add(Producto(3, "individual-premium", "Clases individuales de repaso en informática tamaño premium, 10 clases ", 119.99, 10))
-        productos.add(Producto(4, "grupo-mini", "Clases en grupo de repaso en informática tamaño mini, 2 clases ", 39.99, 2))
-        productos.add(Producto(5, "grupo-basic", "Clases en grupo de repaso en informática tamaño basic, 4 clases ", 59.99, 4))
-        productos.add(Producto(6, "grupo-premium", "Clases en grupo de repaso en informática tamaño premium, 10 clases ", 99.99, 10))
-        productos.add(Producto(7, "proyecto-final", "Tutorización del proyecto de final de grado. 8 clases para conseguir el proyecto que siempre has deseado ", 99.99, 8))
-        productos.add(Producto(8, "PAC-M03A", "Ayuda en la realización de la PAC de desarrollo de M03A ", 49.99, 2))
-        productos.add(Producto(9, "PAC-M03B", "Ayuda en la realización de la PAC de desarrollo de M03B ", 49.99, 2))
-        productos.add(Producto(10, "PAC-DAM-M06", "Ayuda en la realización de la PAC de desarrollo de DAM-M06 ", 49.99, 2))
-        productos.add(Producto(11, "PAC-DAM-M07", "Ayuda en la realización de la PAC de desarrollo de DAM-M07", 49.99, 2))
-        productos.add(Producto(12, "PAC-DAM-M08", "Ayuda en la realización de la PAC de desarrollo de DAM-M08", 49.99, 2))
-        productos.add(Producto(13, "PAC-DAM-M09", "Ayuda en la realización de la PAC de desarrollo de DAM-M09", 49.99, 2))
-        productos.add(Producto(14, "curso-android-1S-2021", "Curso de Android + Kotlin, inicio febrero de 2021", 29.99, 0))
-
-
-        return productos
     }
 
     override fun onAttach(context: Context) {
